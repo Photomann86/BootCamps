f927258706e626441f81fee0cdaf0acb632857ba
Photomann86 <kunzler555@gmail.com>
2023-02-01 22:00:57 +0500

    программа по выводу таблицы умножения исправно работает

---
+++ lesson3/lesson3.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // программа выводит таблицу умножения 
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write(i * j);
+                Console.Write("\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
