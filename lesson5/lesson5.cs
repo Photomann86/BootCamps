9a520241b7da590a64482f3db72e3f485c895d0e
Photomann86 <kunzler555@gmail.com>
2023-02-03 21:31:30 +0500

    создана программа пузырьковой сортировки

---
+++ lesson5/lesson5.cs
@@ -0,0 +1,22 @@
+﻿// Сортировка массива пузырьковым методом"
+Console.WriteLine("введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] massiv = new int[n];
+for(int i = 0; i<n; i++) 
+{
+    Console.WriteLine("введите элементы массива");
+    massiv[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i=0; i<n; i++)
+{
+    for (int j=0; j<n-1; j++)
+    {
+        if (massiv[j]>massiv[j+1])
+        {
+            int temp = massiv[j];
+            massiv[j] = massiv[j+1];
+            massiv[j+1] = temp;
+        }
+    }
+    Console.WriteLine(i + "[" + string.Join(", ", massiv) + "]");
+}
